da0cd599368b11b6335198771b89067132e6f825
jithin0000 <mbjithinbabu@gmail.com>
2020-02-23 11:53:55 +0530

    feed back model added

---
+++ encryptedfile/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from customuser.models import MyUser
+
+def user_file_upload(instance, filename):
+    return "user_{0}/file/{1}".format(instance.user.username, filename)
+
+class EncryptedFile(models.Model):
+    """ encrypted file model """
+    user= models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    file_name=models.CharField(max_length=100)
+    file_url = models.FileField(upload_to=user_file_upload)
+
+    def __str__(self):
+        return self.file_name
